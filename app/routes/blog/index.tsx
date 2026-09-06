97fc93e20a1c7e93c60e5616c750af7b98ebf5dd
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2026-09-06 10:56:00 +0330

    add details post and latest post

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import PostCard from "~/components/PostCard";
 import type { PostMeta } from "~/types";
+import Pagination from "~/components/Pagination";
+import PostFilter from "~/components/PostFilter";
+
 export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
@@ -9,18 +13,53 @@ export async function loader({
   if (!res.ok) throw new Error("Failed to Fetch Data");
 
   const data = await res.json();
+  data.sort((a: PostMeta, b: PostMeta) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
   return { posts: data };
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
-  const { posts } = loaderData;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 3;
 
+  const { posts } = loaderData;
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      post.title.toLowerCase().includes(query) ||
+      post.excerpt.toLowerCase().includes(query)
+    );
+  });
+  const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl text-white mb-8 font-bold">Blogs</h2>
-      {posts.map((post) => (
-        <PostCard post={post} key={post.slug} />
-      ))}
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(query) => {
+          setSearchQuery(query);
+          setCurrentPage(1);
+        }}
+      />
+      <div className="space-y-8">
+        {currentPosts.length === 0 ? (
+          <p className="text-gray-400 text-center">No Posts Found</p>
+        ) : (
+          currentPosts.map((post) => <PostCard post={post} key={post.slug} />)
+        )}
+      </div>
+      {totalPages > 0 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
     </div>
   );
 };
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -4,6 +4,8 @@ import type { Properties } from "~/types";
 import Filtered from "~/components/Filtered";
 import { useState } from "react";
 import Focuses from "~/components/Focuses";
+import type { PostMeta } from "~/types";
+import LatestPosts from "~/components/LatestPosts";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,13 +15,24 @@ export function meta({}: Route.MetaArgs) {
 }
 export async function loader({
   request,
-}: Route.LoaderArgs): Promise<{ properties: Properties[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/properties`);
-  const data = await res.json();
-  return { properties: data };
+}: Route.LoaderArgs): Promise<{ properties: Properties[]; posts: PostMeta[] }> {
+  const url = new URL(request.url);
+  const [projectRes, postRes] = await Promise.all([
+    fetch(`${import.meta.env.VITE_API_URL}/properties`),
+    fetch(new URL("/posts-meta.json", url)),
+  ]);
+  if (!projectRes.ok || !postRes.ok)
+    throw new Error("Failed to Fetch project or posts");
+  const [properties, posts] = await Promise.all([
+    projectRes.json(),
+    postRes.json(),
+  ]);
+
+  // return { properties: data };
+  return { properties, posts };
 }
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
-  const { properties } = loaderData;
+  const { properties, posts } = loaderData;
   const [filterProperties, setFilterProperties] =
     useState<Properties[]>(properties);
   return (
@@ -27,6 +40,7 @@ const HomePage = ({ loaderData }: Route.ComponentProps) => {
       <Filtered properties={properties} onFilter={setFilterProperties} />
       <Featured properties={filterProperties} count={2} />
       <Focuses />
+      <LatestPosts posts={posts} />
     </>
   );
 };
